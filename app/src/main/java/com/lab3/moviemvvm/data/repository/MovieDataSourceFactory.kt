54e21e3d1c72ac062a18adb0940dd3923df03ff0
kut_t <tanikvetil.k@gmail.com>
2020-04-15 14:53:04 +0300

    add lab3

---
+++ app/src/main/java/com/lab3/moviemvvm/data/repository/MovieDataSourceFactory.kt
@@ -0,0 +1,20 @@
+package com.lab3.moviemvvm.data.repository
+
+import androidx.lifecycle.MutableLiveData
+import androidx.paging.DataSource
+import com.lab3.moviemvvm.data.api.TheMovieDBInterface
+import com.lab3.moviemvvm.data.vo.Movie
+import io.reactivex.disposables.CompositeDisposable
+
+class MovieDataSourceFactory (private val apiService : TheMovieDBInterface, private val compositeDisposable: CompositeDisposable)
+    : DataSource.Factory<Int, Movie>() {
+
+    val moviesLiveDataSource =  MutableLiveData<MovieDataSource>()
+
+    override fun create(): DataSource<Int, Movie> {
+        val movieDataSource = MovieDataSource(apiService,compositeDisposable)
+
+        moviesLiveDataSource.postValue(movieDataSource)
+        return movieDataSource
+    }
+}
---
+++ app/src/main/java/com/lab3/moviemvvm/ui/single_movie_details/MovieDetailsRepository.kt
@@ -0,0 +1,29 @@
+package com.lab3.moviemvvm.ui.single_movie_details
+
+import androidx.lifecycle.LiveData
+import com.lab3.moviemvvm.data.api.TheMovieDBInterface
+import com.lab3.moviemvvm.data.repository.MovieDetailsNetworkDataSource
+import com.lab3.moviemvvm.data.repository.NetworkState
+import com.lab3.moviemvvm.data.vo.MovieDetails
+import io.reactivex.disposables.CompositeDisposable
+
+class MovieDetailsRepository (private val apiService : TheMovieDBInterface) {
+
+    lateinit var movieDetailsNetworkDataSource: MovieDetailsNetworkDataSource
+
+    fun fetchSingleMovieDetails (compositeDisposable: CompositeDisposable, movieId: Int) : LiveData<MovieDetails> {
+
+        movieDetailsNetworkDataSource = MovieDetailsNetworkDataSource(apiService,compositeDisposable)
+        movieDetailsNetworkDataSource.fetchMovieDetails(movieId)
+
+        return movieDetailsNetworkDataSource.downloadedMovieResponse
+
+    }
+
+    fun getMovieDetailsNetworkState(): LiveData<NetworkState> {
+        return movieDetailsNetworkDataSource.networkState
+    }
+
+
+
+}
